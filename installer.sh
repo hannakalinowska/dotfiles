a1a92e6badf6cd57e2d805b32e6650a8524a7cab
Hanna Kalinowska <h.kalinowska@nature.com>
2013-11-07 12:34:32 +0000

    Tmuxinator setup

--- installer.sh
+++ installer.sh
@@ -13,5 +13,6 @@ ln -s "$path/.gitignore" ~/.gitignore
 ln -s "$path/.gvimrc" ~/.gvimrc
 ln -s "$path/.vimrc" ~/.vimrc
 ln -s "$path/.tmux.conf" ~/.tmux.conf
+ln -s "$path/.tmuxinator" ~/.tmuxinator
 
 ln -s "$path/.vim/snippets" ~/.vim/snippets
