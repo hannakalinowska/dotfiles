1c9a4093cc3b955574966252926661d6337e0d73
Hanna Kalinowska <hanna.kalinowska@simplybusiness.co.uk>
2018-08-07 09:53:08 +0100

    Installation script

---
+++ install.sh
@@ -0,0 +1,12 @@
+# TODO: install homebrew
+brew install rbenv tmux
+
+rbenv install 2.5.1 && rbenv init
+rbenv local 2.5.1
+gem install bundler
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+brew install hub jq ack ctags
+brew install tree pstree
